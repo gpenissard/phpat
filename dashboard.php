55293339900c26dc4539deb1866eec30e8b87e9a
Gilles Pénissard <gpenissard@gmail.com>
2016-01-04 09:29:07 -0500

    Branchement tableau des messages (source de données fixe et temporaire)

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,26 @@
 <?php
 require_once '_defines.php';
 require_once 'data/_main_data.php';
+require_once 'db/_talkmsg_data.php';
 $site_data[PAGE_ID] = 'dashboard';
 require_once 'view_parts/_page_base.php';
+
 ?>
 
-<div id="main"></div>
+<div id="main">
+    <ul>
+        <?php foreach ($talk_msg_data as $tmsg) { ?>
+            <li class="tmsg_cont" style="background-color: <?php echo $tmsg['tmsg_color'] ?>">
+                <div class="tmsg_head">
+                    <span class="tmsg_time"><?php echo $tmsg['tmsg_time'] ?></span>
+                    <span class="tmsg_username"><?php echo $tmsg['tmsg_user'] ?></span>
+                </div>
+                <p class="tmsg_body"><?php echo $tmsg['tmsg_body'] ?></p>
+            </li>
+
+        <?php } ?>
+    </ul>
+</div>
 
 <?php
 require_once 'view_parts/_page_bottom.php';
---
+++ db/_talkmsg_data.php
@@ -0,0 +1,40 @@
+<?php
+define('TALK_MSG_LIST_SIZE', 25);
+/**
+ * Définir la variable (provisoire) qui contient les derniers
+ * User: gpnissar
+ * Date: 2015-12-28
+ * Time: 22:23
+ */
+$talk_msg_data = array(
+    '11' => array(
+        'tmsg_time' => '2016-01-04 12:15',
+        'tmsg_user' => 'antoinesuper2016',
+        'tmsg_color' => '#ff3fd4',
+        'tmsg_body' => 'Salut à tous. Il y a quelqu\'un en 2016 icitte ?',
+    ),
+    '12' => array(
+        'tmsg_time' => '2016-01-04 12:18',
+        'tmsg_user' => 'olgato',
+        'tmsg_color' => '#2fffda',
+        'tmsg_body' => 'Bonne année, j\'ai préparé des super muffins pour la nouvelle année ! Vous êtes tous invités.',
+    ),
+    '13' => array(
+        'tmsg_time' => '2016-01-04 12:30',
+        'tmsg_user' => 'jeanpapa',
+        'tmsg_color' => '#544eff',
+        'tmsg_body' => 'Des muffins ? Olgato, je vais encore prendre 2 libres là ...',
+    ),
+    '14' => array(
+        'tmsg_time' => '2016-01-04 12:45',
+        'tmsg_user' => 'patenteur',
+        'tmsg_color' => '#fbff3d',
+        'tmsg_body' => 'Je vends 25$ mon tout dernier thème wordpress. Visitez ma page web au www.themesapat.ca',
+    ),
+    '15' => array(
+        'tmsg_time' => '2016-01-04 13:00',
+        'tmsg_user' => 'mohmouaaaahhhhh',
+        'tmsg_color' => '#38ff22',
+        'tmsg_body' => 'Monsieur ça va être l\'heure d\'arrèter là. J\'ai pas assez dor...',
+    ),
+);
