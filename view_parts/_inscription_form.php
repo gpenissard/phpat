79bddc4587aae5f3c05fbe0f00f9664e68cfdf7c
Gilles Pénissard <gpenissard@gmail.com>
2016-01-05 11:50:50 -0500

    Etape ajout validation formulaire inscription

--- view_parts/_inscription_form.php
+++ view_parts/_inscription_form.php
@@ -1,20 +1,79 @@
 <?php
-// Validation ici
+var_dump($_POST); // Inspecter les données POST
+/**
+ * Validation du prenom
+ */
+$prenom_ok = false;
+if (array_key_exists('prenom', $_POST)) {
+    $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_STRING);
+    // Validation du prenom : min 2 caractères
+    $prenom_ok = (1 === preg_match('/^[A-Za-z]{2,}$/', $prenom));
+    var_dump($prenom);
+    var_dump($prenom_ok);
+}
+/**
+ * Validation du nom
+ */
+$nom_ok = false;
+if (array_key_exists('nom', $_POST)) {
+    $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
+    // Validation du nom: min 2 caracteres
+    $nom_ok = (1 === preg_match('/^[A-Za-z]{2,}$/', $nom));
+    var_dump($nom);
+    var_dump($nom_ok);
+}
+/**
+ * Validation du courriel
+ */
+$courriel_ok = false;
+if (array_key_exists('courriel', $_POST)) {
+    $courriel = filter_input(INPUT_POST, 'courriel', FILTER_SANITIZE_EMAIL);
+    $courriel = filter_var($courriel, FILTER_VALIDATE_EMAIL);
+    $courriel_ok = (false !== $courriel);
+    var_dump($courriel);
+    var_dump($courriel_ok);
+}
 
 /**
- * Created by PhpStorm.
- * User: gpnissar
- * Date: 2015-12-16
- * Time: 19:48
+ * Validation du pseudo
  */
+$pseudo_ok = false;
+if (array_key_exists('pseudo', $_POST)) {
+    $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_STRING);
+    // Validation du pseudo : min 4 caractères alpha ou chiffres
+    $pseudo_ok = (1 === preg_match('/^[a-zA-Z0-9]{4,}$/', $pseudo));
+    var_dump($pseudo);
+    var_dump($pseudo_ok);
+}
 
+/**
+ * Validation du mot de passe
+ */
+$mot_passe_ok = false;
+if (array_key_exists('mot-passe', $_POST)) {
+    $mot_passe = filter_input(INPUT_POST, 'mot_passe', FILTER_SANITIZE_STRING);
+    // Validation du mot de passe: alpha, chiffres,caracteres speciaux, min de 4 caracteres
+    $mot_passe_ok = (1 === preg_match('/^[a-zA-Z0-9%&$!*?]{4,}$/', $mot_passe));
+    var_dump($mot_passe);
+    var_dump($mot_passe_ok);
+}
+
+if ($prenom_ok && $nom_ok && $courriel_ok && $pseudo_ok && $mot_passe_ok) {
+    // On enregistre les données et s'en va sur une autre page
+    header("Location:index.php");
+    exit;
+}
 ?>
-
-<form name="inscription" method="post">
-    <label for="firstname">Prénom : </label><input type="text" name="firstname" id="firstname" value="<?php echo isset($_POST['firstname']) ? $_POST['firstname'] : ''; ?>"/><br>
-    <label for="lastname">Nom : </label> <input type="text" name="lastname" id="lastname" value="<?php echo isset($_POST['lastname']) ? $_POST['lastname'] : ''; ?>"/><br>
-    <label for="email">Courriel : </label><input type="text" name="email" id="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>"/><br>
-    <label for="username">Pseudo : </label><input type="text" name="username" id="username" value="<?php echo isset($_POST['username']) ? $_POST['username'] : ''; ?>"/><br>
-    <label for="password">Mot de passe : </label><input type="password" name="password" id="password" value="<?php echo isset($_POST['password']) ? $_POST['password'] : ''; ?>"/><br>
-    <input type="submit" name="subscribe" id="subscribe" value="S'inscrire"/>
+<form id="inscription" name="inscription" xmlns="http://www.w3.org/1999/html" method="post">
+    <label for="prenom">Prénom : </label>
+    <input type="text" name="prenom" id="prenom" value="<?php echo array_key_exists('prenom', $_POST) ? $_POST['prenom'] : '' ?>"/>
+    <label for="nom">Nom : </label>
+    <input type="nom" name="nom" id="nom" value="<?php echo array_key_exists('nom', $_POST) ? $_POST['nom'] : '' ?>"/>
+    <label for="courriel">Courriel : </label>
+    <input type="courriel" name="courriel" id="courriel" value="<?php echo array_key_exists('courriel', $_POST) ? $_POST['courriel'] : '' ?>"/>
+    <label for="pseudo">Pseudo : </label>
+    <input type="pseudo" name="pseudo" id="pseudo" value="<?php echo array_key_exists('pseudo', $_POST) ? $_POST['pseudo'] : '' ?>"/>
+    <label for="mot_passe">Password : </label>
+    <input type="mot_passe" name="mot_passe" id="mot_passe" value="<?php echo array_key_exists('mot_passe', $_POST) ? $_POST['mot_passe'] : '' ?>"/>
+    <input type="submit" name="dologin" id="dologin" value="S'inscrire"/>
 </form>
